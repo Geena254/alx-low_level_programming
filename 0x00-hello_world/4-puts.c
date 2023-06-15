401065b0e3e8cfa1799b114c9e64c6f5ba359d1d
Georgina Njoki Kimani <njokikimani001@gmail.com>
2023-06-15 12:04:32 -0700

    Update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
-#!/bin/bash
 #include <stdio.h>
 /**
  * main - print 'Programming is like building a multilingual puzzle' using puts
  * Return: Always 0 (success), non-zero otherwise
  */
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle")
 	return (0);
