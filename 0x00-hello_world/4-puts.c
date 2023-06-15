56dc95ba1f00226351e33043ecd18f2d2bce00f0
Georgina Njoki Kimani <njokikimani001@gmail.com>
2023-06-15 11:59:22 -0700

    Using Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * main - print 'Programming is like building a multilingual puzzle' using puts
+ * Return: Always 0 (success), non-zero otherwise
+ */
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle")
+	return (0);
+}
