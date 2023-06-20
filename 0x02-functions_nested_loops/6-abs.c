0aacfe0968af690e186866b88d40a953e5671521
Georgina Njoki Kimani <njokikimani001@gmail.com>
2023-06-20 09:05:38 -0700

    abs update

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,14 +3,14 @@
 /**
  * _abs - computes the absolute value of an integer
  * @n: the int to check
- * Return; the absolute value of int
+ * Return: Absolute value of int or zero
  */
 
 int _abs(int n)
 {
-	if (n > 0)
+	if (n < 0)
 	{
 		return (n);
 	}
-	return (-n);
+	return (n);
 }
