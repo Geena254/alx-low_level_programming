d5b3a25d9f12e8ec587b9583f749690f68f725d4
Georgina Njoki Kimani <njokikimani001@gmail.com>
2023-06-22 16:57:40 -0700

    Update

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,36 +8,36 @@
 * Fizz for multiples of 3, Buzz for multiples of 5,
 * and Fizz buzz for multiples of both
 *
-* Return: always0
+* Return: always 0
 */
 
 int main(void)
 {
-int i;
+	int i;
 
-for (i = 1; i <= 100; i++)
-{
-if (i % 15 == 0)
-{
-printf("Fizzbuzz");
-}
-else if (i % 3 == 0)
-{
-printf("Fizz");
-}
-else if (i % 5 == 0)
-{
-printf("Buzz");
-}
-else if (i == 1)
-{
-printf("%d", i);
-}
-else if (i < 100)
-{
-printf(" ");
-}
-}
-printf("\n");
-return (0);
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 15 == 0)
+		{
+			printf("Fizzbuzz");
+		}	
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else if (i == 1)
+		{
+			printf("%d", i);
+		}
+		else if (i < 100)
+		{
+		printf(" ");
+		}
+	}
+	printf("\n");
+	return (0);
 }
