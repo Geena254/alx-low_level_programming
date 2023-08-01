21ab1a1822438507ef358a8c6ff6ecf41f21b853
Georgina Kimani <njokikimani001@gmail.com>
2023-08-01 05:24:34 -0700

    Updated code

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -12,11 +12,12 @@ size_t print_listint(const listint_t *h)
 {
 	size_t cnt = 0;
 
-	do {
+	while (h != NULL)
+	{
 		printf("%d\n", h->n);
 		h = h->next;
 		cnt++;
-	} while (h != NULL);
+	}
 
 	return (cnt);
 }
