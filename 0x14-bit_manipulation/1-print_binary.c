37ba9ee43133a086fb070bcec8817e63ddba7e16
Georgina Kimani <njokikimani001@gmail.com>
2023-08-03 15:32:04 -0700

    Print binary rep

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_binary - Function that prints the binary representation of a number.
+ * @n: The unsigned long int to be converted and printed.
+ * Return: void
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned int i;
+
+	for (i = 1 << 10; i > 0; i = i / 2)
+	{
+		if (n & i)
+		{
+			printf("1");
+		}
+		else
+		{
+			printf("0");
+		}   
+	}
+}
