50b01077f215ba7c35bf253dfbb6a66e029c70a0
Georgina Kimani <njokikimani001@gmail.com>
2023-08-07 17:50:54 -0700

    Update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -5,31 +5,34 @@
  * create_file - Function that reads a text file
  * and prints it to the POSIX standard output.
  * @filename: The name of the file to be read.
- * @text_content: NULL terminated string to write to file
+ * @text_context: NULL terminated string to write to the file
  * Return: 1 on success, -1 on failure.
  */
 
 int create_file(const char *filename, char *text_content)
 {
-	int file_d, bytes_write;
-	int leng = 0;
+	int file, bytes_wr;
+	int leng;
 
 	if (filename == NULL)
 		return (-1);
+
+	file = open("filename", O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (file == -1)
+		return (-1);
+
 	if (text_content != NULL)
 	{
-		while (text_content[leng] != '\0')
+		for (leng = 0; text_content[leng];)
 			leng++;
 		text_content = "";
 	}
 
-	file_d = open(filename, O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR);
-	bytes_write = write(file_d, text_content, leng);
-
-	if (file_d == -1 || bytes_write == -1)
+	bytes_wr = write(file, text_content, leng);
+	if (bytes_wr != leng)
 		return (-1);
 
-	close(file_d);
+	close(file);
 
 	return (1);
 }
