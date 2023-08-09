41485ec935243f34499059620e5b2564cdd30332
Georgina Kimani <njokikimani001@gmail.com>
2023-08-09 14:47:54 -0700

    Update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,18 +17,20 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	file = open("filename", O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
 	if (file == -1)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		for (leng = 0; text_content[leng];)
-			leng++;
+		for (leng = 0; text_content[leng]; leng++)
+			;
 
 		bytes_wr = write(file, text_content, leng);
-		if (bytes_wr != leng)
-		return (-1);
+
+		if (bytes_wr == -1)
+			return (-1);
 	}
 
 	close(file);
