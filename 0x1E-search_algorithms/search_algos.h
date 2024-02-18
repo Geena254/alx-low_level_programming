d9b28d7609b9bf971f0900f02f93f791d4a4cd00
Georgina Kimani <njokikimani001@gmail.com>
2024-02-18 07:32:06 -0800

    addition

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -39,7 +39,7 @@ typedef struct skiplist_s
 } skiplist_t;
 
 listint_t *jump_list(listint_t *list, size_t size, int value);
-int _putchar(c);
+int _putchar(char c);
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
